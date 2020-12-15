a726ce099ce74893cff9207dfab1306b968c8ff7
hasu <hasu_m.s@icloud.com>
2020-12-16 03:09:53 +0900

    [add]#7 connected server

--- Lets-Katikomi/app/src/main/java/com/example/lets_katikomi/MainActivity.kt
+++ Lets-Katikomi/app/src/main/java/com/example/lets_katikomi/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.lets_katikomi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
