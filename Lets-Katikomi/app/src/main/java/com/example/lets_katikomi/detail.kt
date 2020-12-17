917d76335a751734b8050e43f418a41c68d5b2b8
hasu2001610 <hasu_m.s@icloud.com>
2020-12-18 04:27:43 +0900

    [update]add list

--- Lets-Katikomi/app/src/main/java/com/example/lets_katikomi/detail.kt
+++ Lets-Katikomi/app/src/main/java/com/example/lets_katikomi/detail.kt
@@ -3,12 +3,25 @@ package com.example.lets_katikomi
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        // Adapterに渡す配列を作成します
+        val data = arrayOf("共同研究室", "583", "584", "585","593","594","595")
+        // adapterを作成します
+        val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        )
+        // adapterをlistViewに紐付けます。
+        listView.adapter = adapter
     }
 
     fun onButtonTappedMD(view: View?){
